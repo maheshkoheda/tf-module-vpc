cae77d9eeeb3cac92a2ad2f41e9313b95f3f18ba
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-08-25 16:53:24 +0530

    terraform with vpc subnets

--- main.tf
+++ main.tf
@@ -16,6 +16,12 @@ resource "aws_internet_gateway" "igw" {
     Name = "main"
   }
 }
+resource "aws_route" "igw" {
+  for_each = module.subnets["public"]["route_table_ids"]
+  route_table_id            = each.value["id"]
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
 
 output "subnet" {
   value = module.subnets
