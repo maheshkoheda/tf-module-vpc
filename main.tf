529866ada4321cdd8e536cc3392791f03fadc3d0
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-08-28 00:31:12 +0530

    terraform with vpc subnets route table, nat gateways and peering

--- main.tf
+++ main.tf
@@ -56,4 +56,7 @@ resource "aws_route" "peer" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
-
+resource "aws_instance" "main" {
+  instance_type = "t3.micro"
+  image_id = "ami-03265a0778a880afb"
+}
