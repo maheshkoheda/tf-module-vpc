767cbc81c3fc5681829292b9dc6037b9d6995e62
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-08-28 14:32:12 +0530

    terraform with vpc subnets route table, nat gateways and peering

--- main.tf
+++ main.tf
@@ -58,8 +58,8 @@ resource "aws_route" "peer" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
-resource "aws_route" "default-vpc-peer-entry" {
-  route_table_id            = var.default_vpc_route_table_id
-  destination_cidr_block    = var.cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-}
+#resource "aws_route" "default-vpc-peer-entry" {
+#  route_table_id            = var.default_vpc_route_table_id
+#  destination_cidr_block    = var.cidr
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+#}
